5b6283c727199018f9cf3504642908484ac20ef1
Tom Daffin <tom@greenpear.com>
2018-03-26 06:53:56 -0600

    Add a test that does not run with the BarometerTester

---
+++ src/test/kotlin/com/jjtparadox/barometer/otherpackage/test/NotBarometerTest.kt
@@ -0,0 +1,12 @@
+package com.jjtparadox.barometer.otherpackage.test
+
+import org.junit.Test
+
+class NotBarometerTest {
+
+    @Test
+    fun testThatAllJUnitTestsRun() {
+        println("Picked up the jUnit test in the other package!")
+    }
+
+}
